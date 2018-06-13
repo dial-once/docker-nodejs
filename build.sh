083e609bfce306df16e21e2382de13ff8beffc45
mrister <mihovil.rister@gmail.com>
2018-06-13 11:14:19 +0200

    Update tls-carbon to 8.11.3 and add .gitignore

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This script is reference on how to build Node.js images and publis them
+# This script is reference on how to build Node.js images and publish them
 # The Node.js latest image, based on boron currently (Node 6.2.11)
 docker image build --file=Dockerfile --label="Node.js LATEST -> LTS Boron" --pull --tag="dialonce/nodejs:latest" .
 #docker image push dialonce/nodejs:latest
@@ -8,6 +8,6 @@ docker image build --file=Dockerfile --label="Node.js LATEST -> LTS Boron" --pul
 docker image build --file=Dockerfile --label="Node.js LTS Boron" --pull --tag="dialonce/nodejs:lts-boron" .
 #docker image push dialonce/nodejs:lts-boron
 
-# The Node.js LTS CARBON image, based on  Node 8.9.4
+# The Node.js LTS CARBON image, based on  Node 8.9.4 or newer
 docker image build --file=Dockerfile-LTS-Carbon --label="Node.js LTS Carbon" --pull --tag="dialonce/nodejs:lts-carbon" .
 #docker image push dialonce/nodejs:lts-carbon
